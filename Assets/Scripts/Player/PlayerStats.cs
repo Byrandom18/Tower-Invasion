fc05e596c34a766df560e131260b0f30b2a5e506
Byrandom18 <byrandom18@mail.ru>
2025-04-21 20:10:14 +0500

    PlayerStats
    добавил статы и их подсчет в PlayerStats

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -6,12 +6,47 @@ public class PlayerStats : MonoBehaviour
 {
     public float maxHealth = 10;
     public float health;
+    public float healthModifier;
+    public float heathFlat;
+    public float healthBase = 100;
 
+    public float atk;
+    public float atkModifier;
+    public float weaponBase;
+    public float baseAttack = 10;
+    public float atkFlat;
+
+    public float manaBase = 100;
+    public float maxMana;
+    public float manaFlat;
+    public float manaRegenModifier;
+
+
+    public float critChance;
+    public float critDamage;
+    public float defFlat;
+
+
+    public float speedModifier;
+    public float spdModifier;
+    public float baseAttackModifier;
+    
+    
+    
 
     // Start is called before the first frame update
     void Start()
     {
         health = maxHealth;
+    }
+
+    public void UpdateEquipmentStats()
+    {
+        maxHealth = (healthBase * (1 + healthModifier)) + heathFlat;
+        atk = ((baseAttack + weaponBase) * (1 + atkModifier)) + atkFlat;
+        maxMana = manaBase + manaFlat;
+
+
     }
 
     public void TakeDamage(float damage)
