c7fa5a279792bb953055bbb8dad04e59e3d92db6
Byrandom18 <byrandom18@mail.ru>
2025-05-20 01:39:53 +0500

    Prefabs, boss scripts & animations
    добавил префабы плеера, юай, босса, создал новую сцену с боссом, скрипты босса, добавил триггеры в аниматор босса, настроил вызов анимаций, стейт машину босса

---
+++ Assets/Scripts/Mobs/Forest mobs/BOSS/LeshyAnimations.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class LeshyAnimations : MonoBehaviour
+{
+    private Animator animator;
+
+    public bool IsMoving { get; set; }
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        animator.SetBool("IsMoving", IsMoving);
+        
+    }
+
+    public void Slam()
+    {
+        animator.SetTrigger("Slam");
+    }
+
+    public void Bomb()
+    {
+        animator.SetTrigger("Bomb");
+    }
+
+    public void Spikes()
+    {
+        animator.SetTrigger("Spikes");
+    }
+
+    public void Spam()
+    {
+        animator.SetTrigger("Spam");
+    }
+
+    public void Punch()
+    {
+        animator.SetTrigger("Punch");
+    }
+
+    public void Summon()
+    {
+        animator.SetTrigger("Summon");
+    }
+
+    public void Death()
+    {
+        animator.SetTrigger("Death");
+    }
+}
